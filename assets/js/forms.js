814b06e09582494d080e4e0282e7d4de92187895
ali-ulu <aliulu@ai-ulu.com>
2026-09-24 01:49:38 +0300

    feat: remove newsletter signup (it posted nowhere); clip reveal overflow on mobile

--- assets/js/forms.js
+++
@@ -1,4 +0,0 @@
-import {$,$$} from './dom.js';
-
-/* forms */
-$('#newsForm').addEventListener('submit',e=>{e.preventDefault();const i=e.target.querySelector('input');i.value='';i.placeholder='✓ Subscribed!';});
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,3 @@
 import './nav.js';
 import './reveal.js';
 import './effects.js';
-import './forms.js';
